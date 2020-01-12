7aa66bd86fe3d803c8a53d618fb362f50f3aa882
Ayman Osman <aymano.osman@gmail.com>
2020-01-12 22:48:00 +0000

    R1: Add interp-C0

---
+++ siek-interpreter-lib/siek-interpreter/interp-C0.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+(provide interp-C0)
+
+(require racket/fixnum)
+
+(define (interp-C0 p)
+  (match p
+    [`(program () ((start . ,tail)))
+     (interp-tail '() tail)]
+     [_
+       (raise-arguments-error 'interp-C0 "failed match"
+                              "kind" 'top
+                              "term" p)]))
+
+(define (interp-tail env t)
+  (match t
+    [`(return ,e) (interp-exp env e)]
+    [`(seq ,s ,t) (interp-tail (interp-stmt env s) t)]
+    [_
+      (raise-arguments-error 'interp-C0 "failed match"
+                             "kind" 'tail
+                             "term" t)]))
+
+(define (interp-stmt env s)
+  (match s
+    [`(assign ,v ,e) (extend env v (interp-exp env e))]
+    [_
+      (raise-arguments-error 'interp-C0 "failed match"
+        "kind" 'stmt
+        "term" s)]))
+
+(define (interp-exp env e)
+  (match e
+    [(? symbol?) (lookup env e)]
+    [(? fixnum?) e]
+    [`(- ,a) (fx- 0 (interp-exp env a))]
+    [`(+ ,a0 ,a1) (fx+ (interp-exp env a0) (interp-exp env a1))]
+    [`(read) (read-int)]
+    [_
+      (raise-arguments-error 'interp-C0 "failed match"
+        "kind" 'exp
+        "term" e)]))
+
+;; Aux
+
+(define (read-int)
+  (match (read)
+    [(? fixnum? r) r]
+    [other
+      (raise-argument-error 'interp-C0 "integer?" other)]))
+
+(define (lookup env var)
+  (match (assoc var env)
+    [(cons _ value) value]
+    [_
+      (raise-arguments-error var "undefined variable")]))
+
+(define (extend env name value)
+  (cons (cons name value) env))
