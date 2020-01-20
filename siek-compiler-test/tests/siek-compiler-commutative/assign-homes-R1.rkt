dbffdc47b100a23929506d225f6d0a0030674206
Ayman Osman <aymano.osman@gmail.com>
2020-01-20 00:47:29 +0000

    R1: Add commutative tests for assign-homes-pass

---
+++ siek-compiler-test/tests/siek-compiler-commutative/assign-homes-R1.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require tests/siek-compiler-tester/check-pass)
+
+(require siek-compiler/uniquify-pass-R1
+         siek-compiler/remove-complex-opera-pass-R1
+         siek-compiler/explicate-control-pass-R1
+         siek-compiler/uncover-locals-pass-R1
+         siek-compiler/select-instructions-pass-R1
+         siek-compiler/assign-homes-R1)
+
+
+(define compile (compose1 assign-homes-pass-R1
+                          select-instructions-pass-R1
+                          uncover-locals-pass-R1
+                          explicate-control-pass-R1
+                          remove-complex-opera*-pass-R1
+                          uniquify-pass-R1))
+
+(check-pass* compile (R1 -> x860)
+  2
+
+  (- 10)
+
+  (- (+ 10 20))
+
+  (let ([x 32])
+    (+ x 10))
+
+  (let ([x (let ([x 4])
+             (+ x 1))])
+    (+ x 2)))
