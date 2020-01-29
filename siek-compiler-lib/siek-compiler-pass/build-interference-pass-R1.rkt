9d721a29b5d24852282dbdd9f27b1668ef8d34b7
Ayman Osman <aymano.osman@gmail.com>
2020-01-29 18:03:49 +0000

    wip

--- siek-compiler-lib/siek-compiler-pass/build-interference-pass-R1.rkt
+++ siek-compiler-lib/siek-compiler-pass/build-interference-pass-R1.rkt
@@ -1,1 +1,41 @@
 #lang racket
+
+(require graph)
+
+'((v w)
+  (w x y z)
+  (t.1 z))
+
+(define g
+  (unweighted-graph/undirected '((a b) (c d))))
+
+(get-edges g)
+
+(define live-afters
+  (list (set)
+        (set 'v)
+        (set 'v 'w)
+        (set 'w 'x)
+        (set 'w 'x)
+        (set 'w 'x 'y)
+        (set 'w 'x 'y)
+        (set 'w 'y 'z)
+        (set 'y 'z)
+        (set 'z 't.1)
+        (set 'z 't.1)
+        (set 't.1)
+        (set)
+        (set)))
+
+;; naive
+(let loop ([l* (for/list ([x  live-afters]
+                          [y  (rest live-afters)])
+                 (cons x y))]
+           [acc '()])
+  (match l*
+    [(cons (cons a b) l*)
+     (printf "a: ~a\n" a)
+     (printf "b: ~a\n" b)
+     (loop l* acc)]
+    [_
+     acc]))
