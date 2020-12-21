7080a2f51e89c8c188492ffc7ff9348a8d423f14
Ayman Osman <aymano.osman@gmail.com>
2020-12-21 12:36:30 +0000

    fix liveness and interference graph tests

--- tests/siek/build-interference-pass-R1.rkt
+++ tests/siek/build-interference-pass-R1.rkt
@@ -1,72 +1,75 @@
 #lang racket
 
+(provide build-interference-tests)
+
 (require rackunit)
 
 (require siek)
 
 (require graph)
 
-(check-equal?
- (build-interference-pass-R1
-  `(program
-    ()
-    ((start .
-            (block ((live-afters .
-                                 ,(list
-                                   (set)
-                                   (set 'v)
-                                   (set 'v 'w)
-                                   (set 'w 'x)
-                                   (set 'w 'x)
-                                   (set 'w 'x 'y)
-                                   (set 'w 'y 'z)
-                                   (set 'y 'z)
-                                   (set 'z 't.1)
-                                   (set 'z 't.1)
-                                   (set 't.1 't.2)
-                                   (set 't.2)
-                                   (set)
-                                   (set))))
-                   (movq (int 1) (var v))
-                   (movq (int 46) (var w))
-                   (movq (var v) (var x))
-                   (addq (int 7) (var x))
-                   (movq (var x) (var y))
-                   (movq (var x) (var z))
-                   (addq (var w) (var z))
-                   (movq (var y) (var t.1))
-                   (negq (var t.1))
-                   (movq (var z) (var t.2))
-                   (addq (var t.1) (var t.2))
-                   (movq (var t.2) (reg rax))
-                   (jmp conclusion))))))
- `(program
-   ()
-   ((start .
-           (block ((conflicts .
-                              ,(unweighted-graph/undirected
-                                '((w v)
-                                  (x w)
-                                  (x w)
-                                  (y w)
-                                  (z w)
-                                  (z y)
-                                  (z w)
-                                  (z y)
-                                  (t.1 z)
-                                  (t.1 z)
-                                  (t.2 t.1)
-                                  (t.2 t.1)))))
-                  (movq (int 1) (var v))
-                  (movq (int 46) (var w))
-                  (movq (var v) (var x))
-                  (addq (int 7) (var x))
-                  (movq (var x) (var y))
-                  (movq (var x) (var z))
-                  (addq (var w) (var z))
-                  (movq (var y) (var t.1))
-                  (negq (var t.1))
-                  (movq (var z) (var t.2))
-                  (addq (var t.1) (var t.2))
-                  (movq (var t.2) (reg rax))
-                  (jmp conclusion))))))
+(define-test-suite build-interference-tests
+  (test-case "running example"
+    (check-equal?
+     (build-interference-pass-R1
+      `(program
+        ()
+        ((start .
+                (block ((live-afters .
+                                     ,(list
+                                       (set 'v)
+                                       (set 'v 'w)
+                                       (set 'w 'x)
+                                       (set 'w 'x)
+                                       (set 'w 'x 'y)
+                                       (set 'w 'y 'z)
+                                       (set 'y 'z)
+                                       (set 'z 't.1)
+                                       (set 'z 't.1)
+                                       (set 't.1 't.2)
+                                       (set 't.2)
+                                       (set)
+                                       (set))))
+                       (movq (int 1) (var v))
+                       (movq (int 46) (var w))
+                       (movq (var v) (var x))
+                       (addq (int 7) (var x))
+                       (movq (var x) (var y))
+                       (movq (var x) (var z))
+                       (addq (var w) (var z))
+                       (movq (var y) (var t.1))
+                       (negq (var t.1))
+                       (movq (var z) (var t.2))
+                       (addq (var t.1) (var t.2))
+                       (movq (var t.2) (reg rax))
+                       (jmp conclusion))))))
+     `(program
+       ()
+       ((start .
+               (block ((conflicts .
+                                  ,(unweighted-graph/undirected
+                                    '((w v)
+                                      (x w)
+                                      (x w)
+                                      (y w)
+                                      (z w)
+                                      (z y)
+                                      (z w)
+                                      (z y)
+                                      (t.1 z)
+                                      (t.1 z)
+                                      (t.2 t.1)
+                                      (t.2 t.1)))))
+                      (movq (int 1) (var v))
+                      (movq (int 46) (var w))
+                      (movq (var v) (var x))
+                      (addq (int 7) (var x))
+                      (movq (var x) (var y))
+                      (movq (var x) (var z))
+                      (addq (var w) (var z))
+                      (movq (var y) (var t.1))
+                      (negq (var t.1))
+                      (movq (var z) (var t.2))
+                      (addq (var t.1) (var t.2))
+                      (movq (var t.2) (reg rax))
+                      (jmp conclusion))))))))
--- tests/siek/run-all-tests.rkt
+++ tests/siek/run-all-tests.rkt
@@ -8,9 +8,11 @@
          "allocate-registers-pass-R1.rkt"
          "move-related.rkt"
          "color-graph.rkt"
+         "build-interference-pass-R1.rkt"
+         "uncover-locals-pass-R1.rkt"
          "assign-homes-pass-R1.rkt"
          "select-instructions-pass-R1.rkt"
-         "uncover-locals-pass-R1.rkt"
+         "uncover-live-pass-R1.rkt"
          "explicate-control-pass-R1.rkt"
          "remove-complex-opera-pass-R1.rkt"
          "uniquify-pass-R1.rkt"
@@ -24,6 +26,8 @@
              allocate-registers-tests
              move-related-tests
              color-graph-tests
+             build-interference-tests
+             uncover-live-tests
              assign-homes-tests
              select-instructions-tests
              uncover-locals-tests
