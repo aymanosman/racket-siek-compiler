762cfb0e6b2b25678c6f5e964cf5aee1749607a7
Ayman Osman <aymano.osman@gmail.com>
2020-12-23 02:42:07 +0000

    refactor x860? tests

--- tests/siek/x860.rkt
+++ tests/siek/x860.rkt
@@ -2,40 +2,33 @@
 
 (provide x860-tests)
 
-(require rackunit)
+(require rackunit
+         siek)
 
-(require "check-x860.rkt")
-
-(define normal-x86-tests
+(define-test-suite x860-tests
   (test-suite
-   "normal x86"
+   "x86"
    (test-case "valid x86"
-     (check-x860?
+     (check-pred
+      x860?
       '(program
         ()
         ((start .
                 (block ()
                        (movq (int 10) (reg rax))))))))
    (test-case "valid x86"
-     (check-not-x860?
+     (check-pred
+      (compose not x860?)
       '(program
         ()
         ((start .
                 (block ()
-                       (movq (int 10) (var x))))))))))
-
-(define psuedo-x86-tests
-  (test-suite
-   "pseudo x86"
+                       (movq (int 10) (var x))))))))
    (test-case "valid x86*"
-     (check-x860*?
+     (check-pred
+      x860*?
       '(program
         ()
         ((start .
                 (block ()
                        (movq (int 10) (var x))))))))))
-
-(define-test-suite x860-tests
-  (test-suite "x86"
-              normal-x86-tests
-              psuedo-x86-tests))
